11e94c2d40fe32151b375310d7ba8b182d72529b
David Umanah <david_umanah@yahoo.com>
2026-02-13 15:49:17 +0100

    updates

--- src/components/utils/endpoints.ts
+++ src/components/utils/endpoints.ts
@@ -3,4 +3,9 @@ import axios from "axios";
 export const API_URL = process.env.NEXT_PUBLIC_BACK_URL;
 
 export const login = async (data: any) =>
-	axios.post(`${API_URL}/auth/login`, data);
+  axios.post(`${API_URL}/auth/login`, data);
+
+export const trendingEpisodes = async (
+  page: number = 1,
+  perPage: number = 15,
+) => axios.get(`${API_URL}/episodes/trending?page=${page}&per_page=${perPage}`);
