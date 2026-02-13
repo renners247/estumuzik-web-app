02373ec33d57027816501425a30d7bed5b007408
Gideon Adegunle <giddygbenga4@gmail.com>
2026-02-13 15:01:03 +0100

    initialcommit

--- src/components/utils/endpoints.ts
+++ src/components/utils/endpoints.ts
@@ -4,3 +4,8 @@ export const API_URL = process.env.NEXT_PUBLIC_BACK_URL;
 
 export const login = async (data: any) =>
 	axios.post(`${API_URL}/auth/login`, data);
+
+export const trendingEpisodes = async (
+	page: number = 1,
+	perPage: number = 15,
+) => axios.get(`${API_URL}/episodes/trending?page=${page}&per_page=${perPage}`);
